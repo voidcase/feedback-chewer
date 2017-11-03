74bf2f5647bef1d15adac442be2fc8aa1ed76b19
Isak Lindhé <isak.e.lindhe@gmail.com>
2017-11-03 09:38:59 +0100

    refactored dummy creation

--- data_extractor.py
+++ data_extractor.py
@@ -8,14 +8,9 @@ def parse_date(column):
 
 df = pd.read_csv('dataset.csv').drop(['Proposal'], axis = 1)
 
-dummies_beamline = pd.get_dummies(df['Beamline'])
-df = pd.concat([df.drop(['Beamline'], axis=1) ,dummies_beamline], axis = 1)
-
-dummies_department = pd.get_dummies(df['Department']) #adds many columns...
-df = pd.concat([df.drop(['Department'], axis=1) ,dummies_department], axis = 1)
-
-dummies_user_aff = pd.get_dummies(df['User Affiliation']) #adds many columns...
-df = pd.concat([df.drop(['User Affiliation'], axis=1) ,dummies_department], axis = 1)
+for header in ['Beamline', 'Department', 'User Affiliation']:
+    dummies_beamline = pd.get_dummies(df[header])
+    df = pd.concat([df.drop([header], axis=1) ,dummies_beamline], axis = 1)
 
 df['Experiment start'] = parse_date(df['Experiment start'])
 df['Experiment end'] = parse_date(df['Experiment end'])
