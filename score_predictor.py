bfb77197d5a048da84696b31f9ae45fb942ae904
Isak Lindhé <isak.e.lindhe@gmail.com>
2017-11-03 12:38:59 +0100

    cross validation debugging

--- score_predictor.py
+++ score_predictor.py
@@ -1,6 +1,7 @@
 import csv
 from sklearn import linear_model
 from sklearn.model_selection import cross_val_score
+import data_extractor
 
 SCORE_TYPES = [
     'Overall',
@@ -12,24 +13,27 @@ SCORE_TYPES = [
     'DUO'
     ]
 
-def get_column(col):
+def get_int_column(col):
     with open('dataset.csv') as csvfile:
         reader = csv.DictReader(csvfile)
-        return [row[col] for row in reader]
+        return [int(row[col]) for row in reader]
 
 def train_on(scoretype='Overall'):
     if scoretype not in SCORE_TYPES:
         return None
     clf = linear_model.LogisticRegression()
     X = None # TODO: get features
-    y = get_column(scoretype)
+    y = get_int_column(scoretype)
     return clf.fit(X, y)
 
 def print_cross_val_scores(scoretype='Overall'):
     if scoretype not in SCORE_TYPES: return None
     clf = linear_model.LogisticRegression()
-    X = None
-    y = get_column(scoretype)
+    X = data_extractor.data_extract()
+    y = get_int_column(scoretype)
     scores = cross_val_score(clf, X, y)
     print(scores)
+
+if __name__ == '__main__':
+    print_cross_val_scores()
 
