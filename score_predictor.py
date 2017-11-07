2510bcab55563fddb8d69f645cd523cef27f95d7
Isak Lindhé <isak.e.lindhe@gmail.com>
2017-11-07 14:15:59 +0100

    more format cleanup

--- score_predictor.py
+++ score_predictor.py
@@ -1,7 +1,8 @@
 import csv
 from sklearn import linear_model
 from sklearn.model_selection import cross_val_score, train_test_split
-import data_extractor, util
+import data_extractor
+import util
 import matplotlib.pyplot as plt
 from pprint import pprint
 
@@ -13,26 +14,27 @@ def get_int_column(col):
 
 
 def print_cross_val_scores(scoretype='Overall'):
-    if scoretype not in util.SCORE_TYPES: return None
+    if scoretype not in util.SCORE_TYPES:
+        return None
     clf = linear_model.LinearRegression()
-    X = data_extractor.data_extract()
+    x = data_extractor.data_extract()
     y = get_int_column(scoretype)
-    scores = cross_val_score(clf, X, y)
+    scores = cross_val_score(clf, x, y)
     print(scores)
 
 
-def eval():
-    X_all = data_extractor.data_extract()
+def eval_model():
+    x_all = data_extractor.data_extract()
     y_all = get_int_column('Overall')
-    X_train, X_test, y_train, y_test = train_test_split(X_all, y_all, test_size=0.1)
-    model = linear_model.LinearRegression().fit(X_train, y_train)
+    x_train, x_test, y_train, y_test = train_test_split(x_all, y_all, test_size=0.1)
+    model = linear_model.LinearRegression().fit(x_train, y_train)
     coefficients = model.coef_
-    zipped = zip(coefficients, list(X_train))
+    zipped = zip(coefficients, list(x_train))
     pprint(sorted(list(zipped))[-10:])
 
-    # plt.plot(y_test,model.predict(X_test), 'bo')
-    return model.score(X_test, y_test)
+    # plt.plot(y_test,model.predict(x_test), 'bo')
+    return model.score(x_test, y_test)
 
 
 if __name__ == '__main__':
-    print(eval())
+    print(eval_model())
