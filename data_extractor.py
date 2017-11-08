b1bcce30ea3b4848743616e3b5009cf0588d6ec1
Lotta <lotta.meijerink@gmail.com>
2017-11-08 14:11:41 +0100

    variance scaling seems to work

--- data_extractor.py
+++ data_extractor.py
@@ -6,8 +6,9 @@ from sklearn.feature_selection import VarianceThreshold
 from sklearn import preprocessing
 
 
+
 def data_extract():
-    df = pd.read_csv('dataset.csv').drop(util.SCORE_TYPES + ['Proposal'], axis=1)
+    df = pd.read_csv(util.DATASET).drop(util.SCORE_TYPES + ['Proposal'], axis=1)
     df = df.fillna('')
 
     for header in util.NAME_HEADERS:
@@ -23,18 +24,26 @@ def data_extract():
         df[header] = parse_date(df[header])
 
     min_max_scaler = preprocessing.MinMaxScaler()
-    variance_scaler = VarianceThreshold(0.05)
+    variance_scaler = VarianceThreshold(0.005)
     df[dates] = min_max_scaler.fit_transform(df[dates])
     variance_scaler.fit(df)
 
-    remove = variance_scaler.get_support()
-    remove_indices = [x for x in range(len(remove)) if remove[x]]
-    for i in remove_indices:
-        print('dropped', df.columns[i])
-        df.drop(df.columns[i], axis=1)
-    # df = df.iloc[:, lambda df: remove_indices]
+    retain = variance_scaler.get_support()
+    retain_indices = [x for x in range(len(retain)) if retain[x]]
+    #for i in remove_indices:
+        #print('dropped', df.columns[i])
+     #   df.drop(df.columns[i], axis=1)
+    df = df.iloc[:, lambda df: retain_indices]
     return df
 
+def get_all_data():
+    df = pd.read_csv(util.DATASET)
+    return df
+
+def getXandY(y='Overall'):
+    y_col = get_all_data()[y]
+    x = data_extract()
+    return x, y_col
 
 def tfidf(column, columname):
     values = column.values
@@ -50,3 +59,4 @@ def parse_date(column):
     column = pd.to_datetime(column)
     column = [t.value // 10 ** 9 for t in column]
     return column
+
