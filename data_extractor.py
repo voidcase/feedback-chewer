53632b5e813400ccc6d971338ff596062d3b19ba
Lotta <lotta.meijerink@gmail.com>
2017-11-09 09:55:46 +0100

    put supercomment and sep_comments in as functions

--- data_extractor.py
+++ data_extractor.py
@@ -15,30 +15,15 @@ def data_extract():
             dummies = pd.get_dummies(df[header])
             df = pd.concat([df.drop([header], axis=1), dummies], axis=1)
 
-    #supercomment!
-    df['supercomment'] = ""
-    for header in util.TEXT_HEADERS:
-        if header in df:
-            df['supercomment'] += df[header]
-    values = df['supercomment'].values
-    vectorizer = TfidfVectorizer(stop_words='english')
-    tfidf_matrix = vectorizer.fit_transform(values).toarray()
-    featurenames = np.asarray(vectorizer.get_feature_names())
-    tfidf_frame = pd.DataFrame(tfidf_matrix, columns=featurenames)
-    df = df.drop(util.TEXT_HEADERS + ['supercomment'], axis=1)
-    df = pd.concat([df, tfidf_frame], axis=1)
-
-    # for header, suffix in util.TEXT_HEADERS_AND_SUFFIXES:
-    #     if header in df:
-    #         tfidf_frame = tfidf(df[header], suffix)
-    #         df = pd.concat([df.drop([header], axis=1), tfidf_frame], axis=1)
+    df = supercomment(df)
+    #df = sep_comments(df)
 
     for header in util.DATE_HEADERS:
         if header in df:
             df[header] = parse_date(df[header])
 
     min_max_scaler = preprocessing.MinMaxScaler()
-    variance_scaler = VarianceThreshold(0.001)
+    variance_scaler = VarianceThreshold(0.0005)
 
     kept_dates = list(set(util.DATE_HEADERS) - set(util.DROPTEST))
     if kept_dates:
@@ -50,16 +35,28 @@ def data_extract():
     df = df.drop(droppable_headers, axis=1)
     return df
 
-def get_all_data():
-    df = pd.read_csv(util.DATASET)
+def sep_comments(df):
+    for header, suffix in util.TEXT_HEADERS_AND_SUFFIXES:
+        if header in df:
+            tfidf_frame = tfidf_sep_comments(df[header], suffix)
+            df = pd.concat([df.drop([header], axis=1), tfidf_frame], axis=1)
     return df
 
-def getXandY(y='Overall'):
-    y_col = get_all_data()[y]
-    x = data_extract()
-    return x, y_col
+def supercomment(df):
+    df['supercomment'] = ""
+    for header in util.TEXT_HEADERS:
+        if header in df:
+            df['supercomment'] += df[header]
+    values = df['supercomment'].values
+    vectorizer = TfidfVectorizer(stop_words='english', ngram_range=(1,2), min_df=util.MIN_DF)
+    tfidf_matrix = vectorizer.fit_transform(values).toarray()
+    featurenames = np.asarray(vectorizer.get_feature_names())
+    tfidf_frame = pd.DataFrame(tfidf_matrix, columns=featurenames)
+    df = df.drop(util.TEXT_HEADERS + ['supercomment'], axis=1)
+    df = pd.concat([df, tfidf_frame], axis=1)
+    return df
 
-def tfidf(column, columname):
+def tfidf_sep_comments(column, columname):
     values = column.values
     vectorizer = TfidfVectorizer(stop_words='english')
     tfidf_matrix = vectorizer.fit_transform(values).toarray()
@@ -68,6 +65,14 @@ def tfidf(column, columname):
     tfidf_frame.rename(columns=lambda x: x + columname, inplace=True)
     return tfidf_frame
 
+def get_all_data():
+    df = pd.read_csv(util.DATASET)
+    return df
+
+def getXandY(y='Overall'):
+    y_col = get_all_data()[y]
+    x = data_extract()
+    return x, y_col
 
 def parse_date(column):
     column = pd.to_datetime(column)
--- score_predictor.py
+++ score_predictor.py
@@ -15,7 +15,7 @@ def print_cross_val_scores(scoretype='Overall'):
     scores = cross_val_score(clf, x, y , cv=5)
     print("target: ", scoretype, '\ncross_val_scores: ', scores)
 
-def eval_model(scoretype='Overall'):
+def eval_model_LR(scoretype='Overall'):
     x, y = data_extractor.getXandY(scoretype)
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
     model = linear_model.LinearRegression().fit(x_train, y_train)
@@ -25,8 +25,8 @@ def eval_model(scoretype='Overall'):
     zipped = zip(coefficients, list(x_train))
     print("coefficients: " )
     pprint(sorted(list(zipped)))
-    return model.score(x_test, y_test) #coefficient of determination R^2
+    return model.score(x_test, y_test) # coefficient of determination R^2
 
 if __name__ == '__main__':
-    print('score: ', eval_model('Beamline hardware'))
+    print('score: ', eval_model_LR('Beamline hardware'))
     print_cross_val_scores('Beamline hardware')
