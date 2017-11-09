df51a03b47f6024e608f1e1fd1f404df668c4102
Lotta <lotta.meijerink@gmail.com>
2017-11-09 15:17:24 +0100

    please change name if you can think of something :P

---
+++ trashbutnotreally.py
@@ -0,0 +1,16 @@
+def plot_vocab(values)
+    vectorizer = CountVectorizer(stop_words='english')
+    matrix = vectorizer.fit_transform(values).toarray()
+    featurenames = np.asarray(vectorizer.get_feature_names())
+    tfidf_frame = pd.DataFrame(matrix, columns=featurenames)
+    frequencylist = np.zeros(shape=(len(featurenames)))
+    i = 0
+    for column in tfidf_frame:
+        s = sum(tfidf_frame[column])
+        frequencylist[i] = s
+        i += 1
+        if (s > 500):
+            print(column)
+    print(sorted(frequencylist, reverse=True))
+    plt.plot(sorted(frequencylist, reverse=True))
+    plt.show()
