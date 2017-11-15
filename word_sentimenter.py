1888fd16c595719c991a059844e4dc41105e70b1
Lotta <lotta.meijerink@gmail.com>
2017-11-15 14:03:57 +0100

    added method to create dictionary

--- word_sentimenter.py
+++ word_sentimenter.py
@@ -13,27 +13,39 @@ def find_posi_nega_tokens(comment: Comment) -> (list, list):
     return (posi_indices, nega_indices)
 
 
-def find_np_nsubj(comment: Comment, positokens, negatokens):
+def find_nps(comment: Comment, positokens, negatokens) -> (set, set):
     properties = comment.properties
-    subjects = [i for i, w in enumerate(properties) if w == 'nsubj']
+    objects = [i for i, w in enumerate(properties) if w in ['nsubj', 'dobj']]
+    subjects = [i for i, w in enumerate(properties) if w in ['amod']]
     posi, nega = find_posi_nega_tokens(comment)
-    relevant_connections = [comment.connections[i] for i in subjects]
-    positive_nouns = set()
-    negative_nouns = set()
+    relevant_connections = [comment.connections[i] for i in objects] + \
+                           [reversed(comment.connections[j]) for j in subjects]
+    positive_nouns , negative_nouns = set(), set()
     for j, i in relevant_connections:
         for w in posi:
-            if w == i and comment.pos[j] == 'NN':
+            if w == i and comment.pos[j] in ['NN','NNS']:
                 if is_negated(comment, i):
-                    negative_nouns.add(comment.lemmas[j])
+                    negative_nouns = negative_nouns.union(find_compound(comment, j))
                 else:
-                    positive_nouns.add(comment.lemmas[j])
+                    positive_nouns = positive_nouns.union(find_compound(comment, j))
         for w in nega:
-            if w == i and comment.pos[j] == 'NN':
+            if w == i and comment.pos[j] in ['NN','NNS']:
                 if is_negated(comment, i):
-                    positive_nouns.add(comment.lemmas[j])
+                    positive_nouns = positive_nouns.union(find_compound(comment, j))
                 else:
-                    negative_nouns.add(comment.lemmas[j])
+                    negative_nouns = negative_nouns.union(find_compound(comment, j))
     return positive_nouns, negative_nouns
+
+def find_compound(comment: Comment, index) -> set:
+    compounds = [i for i, w in enumerate(comment.properties) if w == 'compound']
+    relevant_connections = [comment.connections[i] for i in compounds]
+    compound = {index}
+    for i, j in relevant_connections:
+        if i == index:
+            compound.add(j)
+        if j == index:
+            compound.add(i)
+    return compound
 
 
 def is_negated(comment: Comment, index):
@@ -44,27 +56,26 @@ def is_negated(comment: Comment, index):
             return True
     return False
 
+def create_dict(comment: Comment):
+    pos_tokens, neg_tokens = find_posi_nega_tokens(comment)
+    pos_nouns, neg_nouns = find_nps(comment, pos_tokens, neg_tokens)
+    dict = {}
+    for i in range(comment.size): dict[i] = 0
+    for i in pos_nouns:
+        dict[i] = 1
+    for i in neg_nouns: dict[i] = -1
+    return dict
 
-def sentiment_annotate(comments):
-    for cmt in comments[:100]:
-        cmts = re.split("[\-!?:.]+", cmt)
-        for cmt in cmts:
-            comment = Comment(cmt)
-            posi, nega = find_posi_nega_tokens(comment)
-            posnouns, negnouns = find_np_nsubj(comment, posi, nega)
-            if (len(negnouns) > 0):
-                print(cmt)
-                print(negnouns)
 
+# cmts = re.split("[\-!?:.]+", cmt)
 
 dataset = data.get_all_data()
 comments = dataset['Experiment comments']
-# c = Comment('kanelbullar are not good')
-# print(c.properties)
-# print(c.connections)
-# print(c.pos)
-# posi, nega = find_posi_nega_tokens(c)
-# print(posi)
-# print(find_np_nsubj(c, posi, nega))
+c = Comment('we did not like the lunch')
+print(create_dict(c))
+print(c.connections)
+print(c.properties)
 
-sentiment_annotate(comments)
+
+
+# sentiment_annotate(comments)
