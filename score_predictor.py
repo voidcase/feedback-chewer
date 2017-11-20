bc96534be7dc33e13aa715689fa11a2740f8ab39
Lotta <lotta.meijerink@gmail.com>
2017-11-20 17:29:29 +0100

    Merge branch 'master' of https://github.com/voidcase/feedback-chewer
    # Conflicts:
    #	data_extractor.py

--- score_predictor.py
+++ score_predictor.py
@@ -1,6 +1,7 @@
 import util
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LinearRegression
 from sklearn import svm
 from embedding_vectorizer import EmbeddingVectorizer
 from data_extractor import data_extract_comments, parse_word_vectors
@@ -8,11 +9,10 @@ from data_extractor import data_extract_comments, parse_word_vectors
 w2v_dict = parse_word_vectors(util.WORDVEC_DATA)
 x, y = data_extract_comments()
 
-vectorizer = EmbeddingVectorizer(w2v_dict)
-# x = vectorizer.transform(x)
 
 svm_w2v = Pipeline([("embedding vectorizer", EmbeddingVectorizer(w2v_dict)),
                     ("svm", svm.SVC())])
+
 
 cv_score = cross_val_score(svm_w2v, x, y)
 print(cv_score)
