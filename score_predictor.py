c37ee46d70525735a8744499581b132a6d82f461
Lotta <lotta.meijerink@gmail.com>
2017-11-20 14:55:17 +0100

    created embedding_vectorizer object

--- score_predictor.py
+++ score_predictor.py
@@ -1,9 +1,12 @@
+import util
 from sklearn.pipeline import Pipeline
 from sklearn import svm
 from embedding_vectorizer import EmbeddingVectorizer
-from data_extractor import get_x_and_y
+from data_extractor import get_x_and_y, parse_word_vectors
 
+w2v_dict = parse_word_vectors(util.WORDVEC_DATA)
 x, y = get_x_and_y()
 
-svm_w2v = Pipeline(["embedding vectorizer", EmbeddingVectorizer(w2v_dict),
+svm_w2v = Pipeline(["tokenizer",
+    "embedding vectorizer", EmbeddingVectorizer(w2v_dict),
                     "svm", svm.SVC()])
