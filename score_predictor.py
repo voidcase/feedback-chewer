33d7399081c8b0eb806753057602249b029ee975
Lotta <lotta.meijerink@gmail.com>
2017-11-23 08:24:23 +0100

    confusion matrix

--- score_predictor.py
+++ score_predictor.py
@@ -1,6 +1,7 @@
 import util
+import numpy as np
 from sklearn.pipeline import Pipeline
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, cross_validate
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -8,11 +9,37 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn import svm
 from embedding_vectorizer import EmbeddingVectorizer
 from data_extractor import data_extract_comments, parse_word_vectors, data_extract_tfidf_comments, create_word_embeddings
+from sklearn.metrics import confusion_matrix, make_scorer
+import seaborn as sn
+import pandas as pd
+import matplotlib.pyplot as plt
 
-def cross_validate() -> dict:
+def tp(y_true, y_pred): return confusion_matrix(y_true, y_pred)[1, 1]
+def tn(y_true, y_pred): return confusion_matrix(y_true, y_pred)[0, 0]
+def fp(y_true, y_pred): return confusion_matrix(y_true, y_pred)[0, 1]
+def fn(y_true, y_pred): return confusion_matrix(y_true, y_pred)[1, 0]
+
+
+def cm(model, x, y) -> list:
+    scoring = {'tp': make_scorer(tp), 'tn': make_scorer(tn), 'fp': make_scorer(fp), 'fn': make_scorer(fn)}
+    cv = cross_validate(model.fit(x,y), x, y, scoring=scoring)
+    mean_tp = round(np.mean(cv['test_tp']))
+    mean_fp = round(np.mean(cv['test_fp']))
+    mean_tn = round(np.mean(cv['test_tn']))
+    mean_fn = round(np.mean(cv['test_fn']))
+    print(mean_tn, mean_fp, mean_tp, mean_fn)
+    df_cm = pd.DataFrame([[mean_tn, mean_fn], [mean_fp, mean_tp]], range(2), range(2))
+    plt.figure(figsize = (10, 7))
+    sn.set(font_scale=1.4)  # for label size
+    sn.heatmap(df_cm, annot=True, annot_kws={"size": 16})  # font size
+    plt.show()
+
+
+def cross_val() -> dict:
     w2v_dict = parse_word_vectors(util.WORDVEC_DATA)
     x, y = data_extract_comments()
     tfidf_matrix = data_extract_tfidf_comments()
+    cm(LogisticRegression(), tfidf_matrix, y)
     #w2v_own_dict = create_word_embeddings(x['supercomment'])
     classifiers = [
         ("logistic regression", LogisticRegression()),
@@ -40,6 +67,8 @@ def cross_validate() -> dict:
         label: cross_val_score(model, x, y, scoring='accuracy')
         for label, model in models_with_word_embedding.items()
     }
+
+
 
     cv_precisions_without = {
         label: cross_val_score(model, tfidf_matrix, y, scoring='accuracy')
