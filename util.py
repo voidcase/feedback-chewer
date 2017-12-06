f69aaded5c99cb7007c0414e18bbd7d3b31e71c7
Lotta <lotta.meijerink@gmail.com>
2017-12-06 16:31:30 +0100

    trying stuff

--- util.py
+++ util.py
@@ -31,7 +31,7 @@ AUTOCORRECT_PICKLE_FILE = 'pickles/autocorrect.pickle'
 DROPTEST = ['Proposal'] + SCORE_TYPES + EXTERNAL_FACTOR_HEADERS + DATE_HEADERS
 TARGET = 'Overall'
 VARIANCE_THRESHOLD = 1
-MIN_DF= 0.01
+MIN_DF= 1
 DEFAULT_CONFIG = {
     'target': 'Overall',
     'variance_threshold': 0.001,
