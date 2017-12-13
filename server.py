95c8aa8df647703d9885b81243badd348ceacce0
Isak Lindhé <isak.e.lindhe@gmail.com>
2017-12-13 16:13:11 +0100

    dead checkboxes

--- server.py
+++ server.py
@@ -6,12 +6,23 @@ from new_hope import statements_with
 from transforms import sentence_split_transform
 import maxiv_data
 from word_sentimenter import find_context
+import pickle
+import util
 import wordset
 
 app = Flask(__name__)
 df = maxiv_data.get_split_set()
 df = sentence_split_transform(df)
-coeffs = get_coeffs()
+coeffs = []
+try:
+    coeffs = pickle.load(open(util.COEFF_PICKLE, 'rb'))
+    print('coeffs from pickle')
+except FileNotFoundError:
+    print('extracting coeffs')
+    coeffs = get_coeffs(['nouns','adjectives'])
+    pickle.dump(coeffs,open(util.COEFF_PICKLE, 'wb'))
+
+
 
 @app.route('/')
 def index():
@@ -32,11 +43,24 @@ def keywords():
 @app.route('/mentions')
 def mentions():
     word = request.args.get('word')
-    stmts = statements_with(word, df)
-    print('found statements')
-    for s in stmts:
-        s['highlights'] = find_context(s['text'], word)
-    print('found contexts')
+    allstmts = {}
+    stmts = {}
+    try:
+        allstmts = pickle.load(open(util.STMTS_PICKLE, 'rb'))
+        if word in allstmts:
+            stmts = allstmts[word]
+        else:
+            raise FileNotFoundError
+        print('loaded stmts from pickle')
+    except FileNotFoundError:
+        print('extracting stmts with contexts and stuff...')
+        stmts = statements_with(word, df)
+        print('found statements')
+        for s in stmts:
+            s['highlights'] = find_context(s['text'], word)
+        print('found contexts')
+        allstmts[word] = stmts
+        pickle.dump(allstmts, open(util.STMTS_PICKLE, 'wb'))
     return jsonify(stmts)
 
 
